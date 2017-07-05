1936e78faefc4dc28e9e672f17582f38eebec01a
joseputin <jsolaro@securitasargentina.com>
2017-07-05 13:17:08 -0300

    Form seleccion relacion

--- TSReports/Views/FormChooseTable.Designer.cs
+++ TSReports/Views/FormChooseTable.Designer.cs
@@ -28,17 +28,19 @@
         private void InitializeComponent()
         {
             this._formChoseColumns_buttonOk = new System.Windows.Forms.Button();
-            this._formChoseColumns_listBoxDestinos = new System.Windows.Forms.ListBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.treeView2 = new System.Windows.Forms.TreeView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this._formChoseColumns_listBoxCampos = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // _formChoseColumns_buttonOk
             // 
-            this._formChoseColumns_buttonOk.Location = new System.Drawing.Point(269, 300);
+            this._formChoseColumns_buttonOk.Location = new System.Drawing.Point(362, 300);
             this._formChoseColumns_buttonOk.Name = "_formChoseColumns_buttonOk";
             this._formChoseColumns_buttonOk.Size = new System.Drawing.Size(75, 23);
             this._formChoseColumns_buttonOk.TabIndex = 0;
@@ -46,72 +48,75 @@
             this._formChoseColumns_buttonOk.UseVisualStyleBackColor = true;
             this._formChoseColumns_buttonOk.Click += new System.EventHandler(this._formChoseColumns_buttonOk_Click);
             // 
-            // _formChoseColumns_listBoxDestinos
+            // treeView1
             // 
-            this._formChoseColumns_listBoxDestinos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this._formChoseColumns_listBoxDestinos.FormattingEnabled = true;
-            this._formChoseColumns_listBoxDestinos.Location = new System.Drawing.Point(6, 19);
-            this._formChoseColumns_listBoxDestinos.Name = "_formChoseColumns_listBoxDestinos";
-            this._formChoseColumns_listBoxDestinos.Size = new System.Drawing.Size(320, 121);
-            this._formChoseColumns_listBoxDestinos.TabIndex = 1;
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(3, 16);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(193, 238);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterCheck);
             // 
             // groupBox1
             // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this._formChoseColumns_listBoxDestinos);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(332, 144);
-            this.groupBox1.TabIndex = 3;
+            this.groupBox1.Size = new System.Drawing.Size(425, 282);
+            this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Relación:";
+            this.groupBox1.Text = "Relación intermedia";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.treeView2);
+            this.groupBox3.Location = new System.Drawing.Point(211, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(205, 257);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Destino:";
+            // 
+            // treeView2
+            // 
+            this.treeView2.CheckBoxes = true;
+            this.treeView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView2.Location = new System.Drawing.Point(3, 16);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.Size = new System.Drawing.Size(199, 238);
+            this.treeView2.TabIndex = 2;
+            this.treeView2.AfterCheck += new System.Windows.Forms.TreeViewEventHandler(this.treeView2_AfterCheck);
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this._formChoseColumns_listBoxCampos);
-            this.groupBox2.Location = new System.Drawing.Point(12, 162);
+            this.groupBox2.Controls.Add(this.treeView1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 19);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(332, 132);
-            this.groupBox2.TabIndex = 4;
+            this.groupBox2.Size = new System.Drawing.Size(199, 257);
+            this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Campos:";
+            this.groupBox2.Text = "Origen:";
             // 
-            // _formChoseColumns_listBoxCampos
-            // 
-            this._formChoseColumns_listBoxCampos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this._formChoseColumns_listBoxCampos.FormattingEnabled = true;
-            this._formChoseColumns_listBoxCampos.Location = new System.Drawing.Point(6, 18);
-            this._formChoseColumns_listBoxCampos.Name = "_formChoseColumns_listBoxCampos";
-            this._formChoseColumns_listBoxCampos.Size = new System.Drawing.Size(320, 108);
-            this._formChoseColumns_listBoxCampos.TabIndex = 2;
-            // 
-            // FormChoseTable
+            // FormChooseTable
             // 
             this.AcceptButton = this._formChoseColumns_buttonOk;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(356, 335);
-            this.Controls.Add(this.groupBox2);
+            this.ClientSize = new System.Drawing.Size(454, 335);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this._formChoseColumns_buttonOk);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "FormChoseTable";
+            this.Name = "FormChooseTable";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Elegir Destino";
-            this.TopMost = true;
+            this.Load += new System.EventHandler(this.FormChooseTable_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -120,9 +125,10 @@
         #endregion
 
         private System.Windows.Forms.Button _formChoseColumns_buttonOk;
-        private System.Windows.Forms.ListBox _formChoseColumns_listBoxDestinos;
+        private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ListBox _formChoseColumns_listBoxCampos;
     }
 }
--- TSReports/Views/FormChooseTable.cs
+++ TSReports/Views/FormChooseTable.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,12 @@ namespace TSReports.Views
 {
     public partial class FormChooseTable : Form
     {
+        public object respuesta;
+        private dynamic resp;
+        private TreeView tree1aux;
+        private TreeView tree2aux;
+        bool tree1checked = false;
+        bool tree2checked = false;
         public FormChooseTable()
         {
             InitializeComponent();
@@ -21,30 +28,212 @@ namespace TSReports.Views
 
         public FormChooseTable(dynamic resp)
         {
-            var list = new ArrayList();
-            foreach (dynamic objresp in resp.data) {
-                int c_origen = objresp["id_campo_origen"];
-                int c_destino = objresp["id_campo_destino"];
-                int t_origen = objresp["id_tabla_origen"];
-                int t_destino = objresp["id_tabla_destino"];
-                string nombre_tabla_origen = objresp["tabla_origen"];
-                string nombre_tabla_destino = objresp["tabla_destino"];
-                string nombre_campo_origen = objresp["campo_origen"];
-                string nombre_campo_destino = objresp["campo_destino"];
-                 
-
-                list.Add(new KeyValuePair<int, string>(t_destino, nombre_tabla_origen + " -> " + nombre_tabla_destino + "[" + nombre_campo_origen + "->" + nombre_campo_destino + "]"));
-            }
+            this.resp = resp;
+            InitializeComponent();
+            
         }
 
         public int idtablaelegida { get; set; }
 
         private void _formChoseColumns_buttonOk_Click(object sender, EventArgs e)
         {
-            this.idtablaelegida = (int)_formChoseColumns_listBoxDestinos.SelectedItem;
-            this.DialogResult = DialogResult.OK;
-            this.Close();
+            object relacionorigen;
+            object relaciondestino;
+            if (tree1checked == true && tree2checked == true)
+            {
+                relacionorigen = new { tabladestino = treeView1.Nodes[0].Tag, campoorigen = treeView1.Nodes[0].Nodes[0].Tag};
+                relaciondestino = new { tabladestino = treeView2.Nodes[0].Tag, campoorigen = treeView2.Nodes[0].Nodes[0].Tag };
+                respuesta = new { relacionorigen, relaciondestino };
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar por lo menos un item de cada lista", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            }
         }
 
+        private void FormChooseTable_Load(object sender, EventArgs e)
+        {
+            var list = new ArrayList();
+            tree1aux = new TreeView();
+            tree2aux = new TreeView();
+
+            foreach (dynamic objresp in resp.data)
+            {
+                if (objresp.discriminador == "origen")
+                {
+                    foreach (dynamic destinos in objresp.destino)
+                    {
+                        TreeNode nododestino = new TreeNode((string)destinos.nombre);
+                        nododestino.Tag = new { id = destinos.id, nombre = destinos.nombre };
+                        int indexdestino = treeView1.Nodes.Add(nododestino);
+                        tree1aux.Nodes.Add((TreeNode)nododestino.Clone());
+                        foreach (dynamic campos in destinos.campos)
+                        {
+                            TreeNode nodocampos = new TreeNode((string)campos.campo_origen + "-" + (string)campos.campo_destino);
+                            nodocampos.Tag = campos;
+                            treeView1.Nodes[indexdestino].Nodes.Add(nodocampos);
+                            tree1aux.Nodes[indexdestino].Nodes.Add((TreeNode)nodocampos.Clone());
+                        }
+                    }
+                    groupBox2.Text = objresp.nombre+" a: ";
+                }
+                else if (objresp.discriminador == "destino")
+                {
+                    foreach (dynamic destinos in objresp.destino)
+                    {
+                        TreeNode nododestino = new TreeNode((string)objresp.nombre);
+                        nododestino.Tag = new { id = destinos.id, nombre = destinos.nombre };
+                        int indexdestino = treeView2.Nodes.Add(nododestino);
+                        tree2aux.Nodes.Add((TreeNode)nododestino.Clone());
+                        groupBox3.Text = destinos.nombre+" desde: " ;
+                        foreach (dynamic campos in destinos.campos)
+                        {
+                            TreeNode nodocampos = new TreeNode((string)campos.campo_origen + "-" + (string)campos.campo_destino);
+                            nodocampos.Tag = campos;
+                            treeView2.Nodes[indexdestino].Nodes.Add(nodocampos);
+                            tree2aux.Nodes[indexdestino].Nodes.Add((TreeNode)nodocampos.Clone());
+                        }
+                    }
+                }
+            }
+            }
+
+        private void treeView1_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            string nombre_padre = "";
+            if (e.Node.Level == 0)
+            {
+                if (tree1checked)
+                {
+                    eliminarseleccion();
+                }
+                return;
+            }
+            if (tree1checked == false)
+            {
+                if (e.Node.Parent != null)
+                {
+                    e.Node.Parent.Checked = true;
+                    nombre_padre = e.Node.Parent.Text;
+
+                }
+                for (int i = 0; i < treeView1.Nodes.Count; i++)
+                {
+                    TreeNode nodo = treeView1.Nodes[i];
+                    if (!nodo.Checked)
+                    {
+                        treeView1.Nodes.Remove(nodo);
+                        i--;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < nodo.Nodes.Count; j++)
+                        {
+                            TreeNode nodohijo = treeView1.Nodes[i].Nodes[j];
+                            if (nodohijo.Checked == false)
+                            {
+                                treeView1.Nodes.Remove(nodohijo);
+                                j--;
+                            }
+                        }
+                    }
+                }
+                tree1checked = true;
+                for (int i = 0; i < treeView2.Nodes.Count; i++)
+                {
+                    if(treeView2.Nodes[i].Text != nombre_padre)
+                    {
+                        TreeNode t2nodo = treeView2.Nodes[i];
+                        treeView2.Nodes.Remove(t2nodo);
+                        i--;
+                    }
+                    else
+                    {
+                        treeView2.Nodes[i].Expand();
+                    }
+                }
+            }
+            else
+            {
+                eliminarseleccion();
+            }
+        }
+
+        private void treeView2_AfterCheck(object sender, TreeViewEventArgs e)
+        {
+            string nombre_padre = "";
+            if (e.Node.Level == 0)
+            {
+                if (tree2checked)
+                {
+                    eliminarseleccion();
+                }
+                return;
+            }
+            if (tree2checked == false)
+            {
+                if (e.Node.Parent != null)
+                {
+                    e.Node.Parent.Checked = true;
+                    nombre_padre = e.Node.Parent.Text;
+                }
+                for (int i = 0; i < treeView2.Nodes.Count; i++)
+                {
+                    TreeNode nodo = treeView2.Nodes[i];
+                    if (!nodo.Checked)
+                    {
+                        treeView2.Nodes.Remove(nodo);
+                        i--;
+                    }
+                    else
+                    {
+                        for (int j = 0; j < nodo.Nodes.Count; j++)
+                        {
+                            TreeNode nodohijo = treeView2.Nodes[i].Nodes[j];
+                            if (nodohijo.Checked == false)
+                            {
+                                treeView2.Nodes.Remove(nodohijo);
+                                j--;
+                            }
+                        }
+                    }
+                }
+                tree2checked = true;
+                for (int i = 0; i < treeView1.Nodes.Count; i++)
+                {
+                    if (treeView1.Nodes[i].Text != nombre_padre)
+                    {
+                        TreeNode t1nodo = treeView1.Nodes[i];
+                        treeView1.Nodes.Remove(t1nodo);
+                        i--;
+                    }
+                    else
+                    {
+                        treeView1.Nodes[i].Expand();
+                    }
+                }
+            }
+            else
+            {
+                eliminarseleccion();
+            }
+        }
+        private void eliminarseleccion()
+        {
+            treeView2.Nodes.Clear();
+            treeView1.Nodes.Clear();
+            foreach (TreeNode nodo in tree2aux.Nodes)
+            {
+                treeView2.Nodes.Add((TreeNode)nodo.Clone());
+            }
+            foreach (TreeNode nodo in tree1aux.Nodes)
+            {
+                treeView1.Nodes.Add((TreeNode)nodo.Clone());
+            }
+            tree1checked = false;
+            tree2checked = false;
+        }
     }
 }
